f8fb30dc1f5720d0e60c9c5934067c1c38157e40
rst-tec <rst.raphael@gmail.com>
2021-05-30 14:38:20 -0300

    Update StockDTO
    Update StockDTO

---
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -0,0 +1,10 @@
+package com.project.bootcamp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+}
